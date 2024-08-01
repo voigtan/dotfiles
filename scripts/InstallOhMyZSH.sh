42159b8f4d409e9d8b43adf206f40ef3b7050fcb
Andreeas Voigt <voigtan@users.noreply.github.com>
2024-08-01 19:50:57 +0200

    update checks

--- scripts/InstallOhMyZSH.sh
+++ scripts/InstallOhMyZSH.sh
@@ -3,13 +3,13 @@
 FOLDER_PATH="$HOME/.oh-my-zsh"
 
 if [ -d "$FOLDER_PATH" ]; then
-  echo "Oh My Zsh is already installed in $FOLDER_PATH"
-  # use the ask function from the tools folder 
-  if ask "Do you want to uninstall and reinstall Oh My Zsh?" N; then
-    echo "installing"
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/uninstall.sh)"
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  fi
+    echo "Oh My Zsh is already installed in $FOLDER_PATH"
+    # use the ask function from the tools folder 
+    if ask "Do you want to uninstall and reinstall Oh My Zsh?" N; then
+        echo "installing"
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/uninstall.sh)"
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    fi
 else
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
