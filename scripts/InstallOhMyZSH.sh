0dd61373b10aa16d774ab243cb4c41a8d80523ae
Andreeas Voigt <voigtan@users.noreply.github.com>
2024-08-01 17:00:57 +0200

    update scripts

--- scripts/InstallOhMyZSH.sh
+++ scripts/InstallOhMyZSH.sh
@@ -1,4 +1,15 @@
-#!/bin/bash
-set -ex
-# Install Oh-my-zsh https://github.com/ohmyzsh/ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#!/usr/bin/env bash
+
+FOLDER_PATH="$HOME/.oh-my-zsh"
+
+if [ -d "$FOLDER_PATH" ]; then
+  echo "Oh My Zsh is already installed in $FOLDER_PATH"
+  # use the ask function from the tools folder 
+  if ask "Do you want to uninstall and reinstall Oh My Zsh?" N; then
+    echo "installing"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/uninstall.sh)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  fi
+else
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
