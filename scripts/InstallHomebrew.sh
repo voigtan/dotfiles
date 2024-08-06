8466b873e42d7c522d2e7b2f11b5460109f8e949
Andreeas Voigt <voigtan@users.noreply.github.com>
2024-08-06 13:11:40 +0200

    chore: Update Homebrew installation script

--- scripts/InstallHomebrew.sh
+++ scripts/InstallHomebrew.sh
@@ -6,7 +6,6 @@ if ! appExists brew; then
     # Install Homebrew
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
 
     ok "Homebrew installed"
