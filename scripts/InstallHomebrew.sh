657db11574815a355784115683a33799613a4ad8
Andreeas Voigt <voigtan@users.noreply.github.com>
2024-08-06 10:51:21 +0200

    add brew to zprofile

--- scripts/InstallHomebrew.sh
+++ scripts/InstallHomebrew.sh
@@ -5,6 +5,10 @@ source ./tools/scripts.sh
 if ! appExists brew; then
     # Install Homebrew
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+
     ok "Homebrew installed"
 else
     ok "Homebrew Already installed"
