dafaf73caa1af1944d71ead0901232011827ae21
Andreeas Voigt <voigtan@users.noreply.github.com>
2024-08-06 09:39:42 +0200

    update checks and use ok function to give feedback to user

--- scripts/InstallOhMyZSH.sh
+++ scripts/InstallOhMyZSH.sh
@@ -1,15 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
+
+source ./tools/scripts.sh
 
 FOLDER_PATH="$HOME/.oh-my-zsh"
 
 if [ -d "$FOLDER_PATH" ]; then
-    echo "Oh My Zsh is already installed in $FOLDER_PATH"
-    # use the ask function from the tools folder 
-    if ask "Do you want to uninstall and reinstall Oh My Zsh?" N; then
-        echo "installing"
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/uninstall.sh)"
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-    fi
+    ok "Oh My Zsh is already installed in $FOLDER_PATH"
 else
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    ok "Oh My Zsh installed successfully"
 fi
