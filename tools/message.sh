7926e4e3335cbe8aa2fbedbcb5dda49c9b9b839e
Andreeas Voigt <voigtan@users.noreply.github.com>
2024-08-06 09:40:22 +0200

    add banner

--- tools/message.sh
+++ tools/message.sh
@@ -20,3 +20,18 @@ error_exit() {
 pause() {
   read \?'Press any key to continue or Ctrl+C to exit...'
 }
+
+function banner() {
+    color=36
+    color2=35
+
+    echo "\e[0;${color}m            .__        __ "
+    echo "___  ______ |__| _____/  |______    ____ "
+    echo "\  \/ /  _ \|  |/ ___\   __\__  \  /    \ "
+    echo " \   (  <_> )  / /_/  >  |  / __ \|   |  \ "
+    echo "  \_/ \____/|__\___  /|__| (____  /___|  /"
+    echo "              /_____/ \e[0;${color2}m ┓   ┏•┓ \e[0;${color}m \/     \/ \e[0;${color2}m"
+    echo "                      ┏┫┏┓╋╋┓┃┏┓┏"
+    echo "                      ┗┻┗┛┗┛┗┗┗ ┛"
+    echo "\e[0m"
+}
