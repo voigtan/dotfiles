49daed6321119bfe72125d8fde97c41ecebcc65f
Andreeas Voigt <voigtan@users.noreply.github.com>
2024-11-11 16:46:47 +0100

    update brewfile and add new docker compose version, not sure I will use DockerPlugins.sh in the long run

--- scripts/DockerPlugins.sh
+++ scripts/DockerPlugins.sh
@@ -1,5 +1,5 @@
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker} \
   && mkdir -p $DOCKER_CONFIG/cli-plugins \
-  && curl -SL https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-darwin-aarch64 -o $DOCKER_CONFIG/cli-plugins/docker-compose \
+  && curl -SL https://github.com/docker/compose/releases/download/v2.30.2/docker-compose-darwin-aarch64 -o $DOCKER_CONFIG/cli-plugins/docker-compose \
   && chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose \
   && docker compose version
