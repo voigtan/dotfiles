0b119af497ee64b6ac537bb3e6ed5817dcee4218
Andreeas Voigt <voigtan@users.noreply.github.com>
2024-12-05 10:49:36 +0100

    symlink

--- scripts/DockerPlugins.sh
+++ scripts/DockerPlugins.sh
@@ -1,5 +1,4 @@
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker} \
   && mkdir -p $DOCKER_CONFIG/cli-plugins \
-  && curl -SL https://github.com/docker/compose/releases/download/v2.30.2/docker-compose-darwin-aarch64 -o $DOCKER_CONFIG/cli-plugins/docker-compose \
-  && chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose \
-  && docker compose version
+  && ln -sfn $(brew --prefix)/opt/docker-compose/bin/docker-compose ~/.docker/cli-plugins/docker-compose \
+  && ln -sfn $(brew --prefix)/opt/docker-buildx/bin/docker-buildx ~/.docker/cli-plugins/docker-buildx
